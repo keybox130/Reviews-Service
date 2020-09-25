014a120f3f4739003f7d084f60d9fe05b115aaec
Chris Bell <chrisbell.dev@gmail.com>
2020-09-25 12:45:23 -0700

    Implement code review suggestions
    Round ratings to tenth decimal places, only seed database once and other misc changes

--- server/db.js
+++ server/db.js
@@ -14,10 +14,17 @@ const config = {
   dropDatabase: true,
 };
 
-// get random number between min and max, inclusive
+// get random int between min and max, inclusive
 const getRandomInt = (min, max) => {
   let range = max - min;
   return Math.floor(Math.random() * range) + min;
+}
+
+// get random float between min and max, inclusive and rounded to numDecimalPlaces
+const getRandomFloat = (min, max) => {
+  const numDecimalPlaces = 2;
+  let range = max - min;
+  return Number(((Math.random() * range) + min).toFixed(numDecimalPlaces));
 }
 
 const randomNames = ['Chris', 'Katie', 'Emmanuel', 'Josef', 'Kiara', 'Karen', 'Clarence', 'Jorge', 'Antonio',
@@ -31,15 +38,16 @@ const randomText = ['apple', 'orange', 'pear', 'little', 'big', 'hack', 'reactor
                      'is', 'of', 'will', 'tree', 'normal', 'abstract', 'your', 'job', 'free', 'work', 'stay','home',
                      'apartment', 'kite', 'rent', 'stay', 'like', 'roof', 'room', 'bathroom', 'lorem ipsum'];
 
-const generateRandomReview = (length) => {
-  let review = '';
-  for (let i = 0; i < length; i++) {
-    let index = Math.floor(Math.random() * randomText.length);
-    // console.log(index);
-    review += randomText[index] + ' ';
+const generateRandomReview = () => {
+  let review = [];
+  // generate random review of length between 50-150
+  let reviewLength = getRandomInt(50,150);
+  for (let i = 0; i < reviewLength; i++) {
+    // get a random word from randomText
+    let index = getRandomInt(0, randomText.length);
+    review.push(randomText[index]);
   }
-  review = review.slice(0, review.length - 1);
-  return review;
+  return review.join(' ');
 };
 
 // generate a random document
@@ -50,12 +58,12 @@ const generateRandomDocument = (id) => {
     date: new Date(),
     name: randomNames[getRandomInt(0, randomNames.length)],
     rating: {
-      cleanliness: getRandomInt(1, 5),
-      communication: getRandomInt(1, 5),
-      checkIn: getRandomInt(1, 5),
-      accuracy: getRandomInt(1, 5),
-      location: getRandomInt(1, 5),
-      value: getRandomInt(1, 5)
+      cleanliness: getRandomFloat(1, 5),
+      communication: getRandomFloat(1, 5),
+      checkIn: getRandomFloat(1, 5),
+      accuracy: getRandomFloat(1, 5),
+      location: getRandomFloat(1, 5),
+      value: getRandomFloat(1, 5)
     }
   };
   return doc;
@@ -78,30 +86,40 @@ const reviewSchema = new mongoose.Schema({
   }
 });
 
+const Review = mongoose.model('Review', reviewSchema);
+
 const initDB = () => {
 
   let documents = [];
   const numDocuments = 100;
 
-  const Review = mongoose.model('Review', reviewSchema);
-
+  // generate 'numDocuments' random documents
   for (let i = 0; i < numDocuments; i++) {
     const doc = generateRandomDocument(i+1);
     documents.push(doc);
   }
 
+  // insert them and return the promise
   return Review.insertMany(documents);
 
 };
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // populate the database once we are connected
-  initDB()
-    .then(docs => {
-      console.log('Successfully initialized database');
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  // populate the database once we are connected (if necessary!)
+  Review.find({}).exec()
+  .then(docs => {
+    // check if there are already docs in the database
+    if (!docs.length) {
+      initDB()
+        .then(docs => {
+          console.log('Successfully initialized database');
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    } else {
+      console.log('Database is already populated!');
+    }
+  });
 });
