7f115881238bab64fb5a6f68a48931e9a6e23c9b
Chris Bell <cb.camellia@gmail.com>
2021-01-19 23:03:14 -0800

    Add support for cached database, locally hosted assets

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const compression = require('compression');
-const db = require('./db.js');
+const db = require('./db/redis');
 const path = require('path');
+const redis = require('redis').createClient();
 
 const app = express();
 const port = 3003;
 app.use(compression());
 
+db.load();
+
 // send html, bundles, and fonts/stylesheets
 app.use(express.static(path.join(__dirname, '..', '..', 'public/fonts')));
 app.use(express.static(path.join(__dirname, '..', '..', 'public/dist')));
+app.use(express.static(path.join(__dirname, '..', '..', 'public/db')));
 
 // return a stay matching the provided roomId
 app.get('/reviews/stays/:id', (req, res) => {
   const { id } = req.params;
-  db.Room.find({ room_id: id })
-    .exec()
-    .then((room) => {
-      res.status(200).send(room[0]);
-    })
-    .catch((err) => {
-      res.status(404).send(err);
-    });
+  // db.Room.find({ room_id: id })
+  //   .exec()
+  //   .then((room) => {
+  //     res.status(200).send(room[0]);
+  //   })
+  //   .catch((err) => {
+  //     res.status(404).send(err);
+  //   });
 });
 
 app.listen(port, () => {
